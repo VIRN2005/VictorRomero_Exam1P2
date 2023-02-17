f097524ba4d8dd7827c9c6d404abea0fe793728d
VIRN2005 <virn2005@gmail.com>
2023-02-17 15:08:07 -0600

    Creación de Clases con Herencia

---
+++ src/presentacion/Laptops.java
@@ -0,0 +1,40 @@
+package presentacion;
+
+public class Laptops extends PC {
+
+    private String Marca;
+    private String defPant;
+    private boolean RGB;
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getDefPant() {
+        return defPant;
+    }
+
+    public void setDefPant(String defPant) {
+        this.defPant = defPant;
+    }
+
+    public boolean isRGB() {
+        return RGB;
+    }
+
+    public void setRGB(boolean RGB) {
+        this.RGB = RGB;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca: " + Marca + "\n"
+                + "Definicion Pantalla: " + defPant + "\n"
+                + "RGB=" + RGB;
+    }
+
+}
---
+++ src/presentacion/PC_Escritorio.java
@@ -0,0 +1,62 @@
+package presentacion;
+
+public class PC_Escritorio extends PC {
+
+    private int RAM;
+    private int almacenamiento;
+    private int tipoAlmacenamiento;
+    private boolean tarjGrafica;
+
+    public PC_Escritorio() {
+        super();
+    }
+
+    public PC_Escritorio(int RAM, int almacenamiento, int tipoAlmacenamiento, boolean tarjGrafica, String hostName, String direccionIP, String maskRed) {
+        super(hostName, direccionIP, maskRed);
+        this.RAM = RAM;
+        this.almacenamiento = almacenamiento;
+        this.tipoAlmacenamiento = tipoAlmacenamiento;
+        this.tarjGrafica = tarjGrafica;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(int RAM) {
+        this.RAM = RAM;
+    }
+
+    public int getAlmacenamiento() {
+        return almacenamiento;
+    }
+
+    public void setAlmacenamiento(int almacenamiento) {
+        this.almacenamiento = almacenamiento;
+    }
+
+    public int getTipoAlmacenamiento() {
+        return tipoAlmacenamiento;
+    }
+
+    public void setTipoAlmacenamiento(int tipoAlmacenamiento) {
+        this.tipoAlmacenamiento = tipoAlmacenamiento;
+    }
+
+    public boolean isTarjGrafica() {
+        return tarjGrafica;
+    }
+
+    public void setTarjGrafica(boolean tarjGrafica) {
+        this.tarjGrafica = tarjGrafica;
+    }
+
+    @Override
+    public String toString() {
+        return "RAM: " + RAM + "\n"
+                + "Almacenamiento: " + almacenamiento + "\n"
+                + "Tipo de Almacenamiento: " + tipoAlmacenamiento + "\n"
+                + "Tarjeta Grafica: " + tarjGrafica;
+    }
+
+}
