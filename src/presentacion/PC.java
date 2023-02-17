ef8edaaf5f9bf9341ea6fad4d9f6843dc62bc4e4
VIRN2005 <virn2005@gmail.com>
2023-02-17 14:43:21 -0600

    Primer commit del  Gui

---
+++ src/presentacion/PC.java
@@ -0,0 +1,49 @@
+package presentacion;
+
+public class PC {
+
+    private String hostName;
+    private String direccionIP;
+    private String maskRed;
+
+    public PC() {
+    }
+
+    public PC(String hostName, String direccionIP, String maskRed) {
+        this.hostName = hostName;
+        this.direccionIP = direccionIP;
+        this.maskRed = maskRed;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getDireccionIP() {
+        return direccionIP;
+    }
+
+    public void setDireccionIP(String direccionIP) {
+        this.direccionIP = direccionIP;
+    }
+
+    public String getMaskRed() {
+        return maskRed;
+    }
+
+    public void setMaskRed(String maskRed) {
+        this.maskRed = maskRed;
+    }
+
+    @Override
+    public String toString() {
+        return "HostName: " + hostName + "\n"
+                + "Direccion IP: " + direccionIP + "\n"
+                + "Mascara de Red=" + maskRed + '}';
+    }
+
+}
